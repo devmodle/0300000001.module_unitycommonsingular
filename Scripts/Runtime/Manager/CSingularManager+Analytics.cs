743b67b553d9e73da4501373d7ccc6a5f08b000e
sd.lee <sd.lee@ninetap.com>
2020-07-31 18:39:54 +0900

    [이상동] 패키지 설정 파일 추가

---
+++ Scripts/Runtime/Manager/CSingularManager+Analytics.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if SINGULAR_ENABLE && SINGULAR_ANALYTICS_ENABLE
+#if PURCHASE_ENABLE
+using UnityEngine.Purchasing;
+#endif			// #if PURCHASE_ENABLE
+
+//! 싱귤러 관리자 - 분석
+public partial class CSingularManager : CSingleton<CSingularManager> {
+	#region 함수
+	//! 분석 유저 식별자를 변경한다
+	public void SetAnalyticsUserID(string a_oID) {
+		CAccess.Assert(a_oID.ExIsValid());
+		CFunc.ShowLog("CSingularManager.SetAnalyticsUserID: {0}", KCDefine.B_LOG_COLOR_PLUGIN, a_oID);
+
+		if(this.IsInit) {
+			SingularSDK.SetCustomUserId(a_oID);
+		}
+	}
+
+	//! 로그를 전송한다
+	public void SendLog(string a_oName) {
+		this.SendLog(a_oName, null);
+	}
+
+	//! 로그를 전송한다
+	public void SendLog(string a_oName, string a_oParam, List<string> a_oDataList) {
+		CAccess.Assert(a_oParam.ExIsValid());
+
+		this.SendLog(a_oName, new Dictionary<string, object>() {
+			[a_oParam] = a_oDataList.ExToString(KCDefine.B_TOKEN_CSV_STRING)
+		});
+	}
+
+	//! 로그를 전송한다
+	public void SendLog(string a_oName, Dictionary<string, object> a_oDataList) {
+		CAccess.Assert(a_oName.ExIsValid());
+		CFunc.ShowLog("CSingularManager.SendLog: {0}, {1}", KCDefine.B_LOG_COLOR_PLUGIN, a_oName, a_oDataList);
+
+#if ANALYTICS_TEST_ENABLE || (ADHOC_BUILD || STORE_BUILD)
+		if(this.IsInit) {
+			var oDataList = a_oDataList ?? new Dictionary<string, object>();
+
+#if MSG_PACK_ENABLE
+			oDataList.ExAddValue(KCDefine.U_LOG_KEY_DEVICE_ID, CAppInfoStorage.Instance.AppInfo.DeviceID);
+
+#if AUTO_LOG_PARAM_ENABLE
+			oDataList.ExAddValue(KCDefine.U_LOG_KEY_PLATFORM, CAppInfoStorage.Instance.PlatformName);
+			oDataList.ExAddValue(KCDefine.U_LOG_KEY_USER_TYPE, CUserInfoStorage.Instance.UserInfo.UserType.ToString());
+			
+			oDataList.ExAddValue(KCDefine.U_LOG_KEY_LOG_TIME, System.DateTime.UtcNow.ExToLongString());
+			oDataList.ExAddValue(KCDefine.U_LOG_KEY_INSTALL_TIME, CAppInfoStorage.Instance.AppInfo.UTCInstallTime.ExToLongString());
+#endif			// #if AUTO_LOG_PARAM_ENABLE
+#endif			// #if MSG_PACK_ENABLE
+
+			SingularSDK.Event(oDataList, a_oName);
+		}
+#endif			// #if ANALYTICS_TEST_ENABLE || (ADHOC_BUILD || STORE_BUILD)
+	}
+	#endregion			// 함수
+
+	#region 조건부 함수
+#if PURCHASE_ENABLE
+	//! 결제 로그를 전송한다
+	public void SendPurchaseLog(Product a_oProduct, Dictionary<string, object> a_oDataList) {
+		CFunc.ShowLog("CSingularManager.SendPurchaseLog: {0}", KCDefine.B_LOG_COLOR_PLUGIN, a_oProduct);
+
+#if ANALYTICS_TEST_ENABLE || (ADHOC_BUILD || STORE_BUILD)
+		if(this.IsInit) {
+			SingularSDK.InAppPurchase(myProduct, a_oDataList);
+		}
+#endif			// #if ANALYTICS_TEST_ENABLE || (ADHOC_BUILD || STORE_BUILD)
+	}
+#endif			// #if PURCHASE_ENABLE
+	#endregion			// 조건부 함수
+}
+#endif			// #if SINGULAR_ENABLE && SINGULAR_ANALYTICS_ENABLE
---
+++ Scripts/Runtime/Manager/CSingularManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if SINGULAR_ENABLE
+//! 싱귤러 관리자
+public partial class CSingularManager : CSingleton<CSingularManager> {
+	#region 프로퍼티
+	public bool IsInit { get; private set; } = false;
+	#endregion			// 프로퍼티 
+
+	#region 함수
+	//! 초기화
+	public virtual void Init(System.Action<CSingularManager, bool> a_oCallback) {
+		CFunc.ShowLog("CSingularManager.Init");
+
+		if(!this.IsInit && CAccess.IsMobilePlatform()) {
+			this.IsInit = true;
+			SingularSDK.InitializeSingularSDK();
+		}
+
+		a_oCallback?.Invoke(this, this.IsInit);
+	}
+	#endregion			// 함수
+}
+#endif			// #if SINGULAR_ENABLE
