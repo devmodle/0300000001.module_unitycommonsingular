5e185114d69264f9f486fdf9ff9b1e762ebd83ee
dante <are2341@nate.com>
2020-10-26 14:15:57 +0900

    [이상동] 로그 관련 로직 수정

--- Scripts/Runtime/Manager/CSingularManager+Analytics.cs
+++ Scripts/Runtime/Manager/CSingularManager+Analytics.cs
@@ -60,7 +60,7 @@ public partial class CSingularManager : CSingleton<CSingularManager> {
 
 #if AUTO_LOG_PARAMS_ENABLE
 			oDataList.ExAddValue(KCDefine.U_LOG_KEY_PLATFORM, 
-				CCommonAppInfoStorage.Instance.PlatformName);
+				CCommonAppInfoStorage.Instance.Platform);
 
 			oDataList.ExAddValue(KCDefine.U_LOG_KEY_USER_TYPE, 
 				CCommonUserInfoStorage.Instance.UserInfo.UserType.ToString());
