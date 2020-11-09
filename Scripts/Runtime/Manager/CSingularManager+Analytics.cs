63c16d28734def4baa8e2bf2f11e673495461fe4
dante <are2341@nate.com>
2020-11-10 08:59:34 +0900

    [이상동] 서브 모듈 갱신

--- Scripts/Runtime/Manager/CSingularManager+Analytics.cs
+++ Scripts/Runtime/Manager/CSingularManager+Analytics.cs
@@ -39,9 +39,9 @@ public partial class CSingularManager : CSingleton<CSingularManager> {
 	}
 
 	//! 로그를 전송한다
-	public void SendLog(string a_oName, string a_oParam, List<string> a_oDataList) {
+	public void SendLog(string a_oName, string a_oParams, List<string> a_oDataList) {
 		this.SendLog(a_oName, new Dictionary<string, object>() {
-			[a_oParam] = a_oDataList.ExToString(KCDefine.B_TOKEN_CSV_STRING)
+			[a_oParams] = a_oDataList.ExToString(KCDefine.B_TOKEN_CSV_STRING)
 		});
 	}
 
