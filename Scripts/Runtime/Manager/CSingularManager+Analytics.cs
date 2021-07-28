7ffb1345a07ddd8d70b57c6147299d87ef06d7af
dante <are2341@nate.com>
2021-07-29 00:01:41 +0900

    [이상동] 서브 모듈 갱신

--- Scripts/Runtime/Manager/CSingularManager+Analytics.cs
+++ Scripts/Runtime/Manager/CSingularManager+Analytics.cs
@@ -45,7 +45,7 @@ public partial class CSingularManager : CSingleton<CSingularManager> {
 #if (SINGULAR_ANALYTICS_ENABLE && (UNITY_IOS || UNITY_ANDROID)) && (ANALYTICS_TEST_ENABLE || (ADHOC_BUILD || STORE_BUILD))
 		// 초기화 되었을 경우
 		if(this.IsInit) {
-			var oDataDict = a_oDataDict ?? new Dictionary<string, object>();
+			var oDataDict = (a_oDataDict != null) ? a_oDataDict : new Dictionary<string, object>();
 
 			oDataDict.ExAddVal(KCDefine.L_LOG_KEY_DEVICE_ID, CCommonAppInfoStorage.Inst.AppInfo.DeviceID);
 			oDataDict.ExAddVal(KCDefine.L_LOG_KEY_PLATFORM, CCommonAppInfoStorage.Inst.Platform);
